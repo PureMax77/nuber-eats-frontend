9cf9053cee15b52c169bc15897f649869822ea86
ertr <ertr@coinplug.com>
2023-05-23 10:17:53 +0900

    Owner MyRestaurants 추가

---
+++ src/pages/owner/my-restaurants.tsx
@@ -0,0 +1,3 @@
+export const MyRestaurants = () => {
+  return <div>나의 레스토랑</div>;
+};
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -9,26 +9,43 @@ import { EditProfile } from "../pages/user/edit-profile";
 import { Search } from "../pages/client/search";
 import { Category } from "../pages/client/category";
 import { Restaurant } from "../pages/client/restaurant";
+import { MyRestaurants } from "../pages/owner/my-restaurants";
 
-const ClientRouter = [
-  <Route key={1} path="/" exact>
-    <Restaurants />
-  </Route>,
-  <Route key={2} path="/confirm" exact>
-    <ConfirmEmail />
-  </Route>,
-  <Route key={3} path="/edit-profile" exact>
-    <EditProfile />
-  </Route>,
-  <Route key={4} path="/search" exact>
-    <Search />
-  </Route>,
-  <Route key={5} path="/category/:slug" exact>
-    <Category />
-  </Route>,
-  <Route key={6} path="/restaurants/:id" exact>
-    <Restaurant />
-  </Route>,
+const clientRoutes = [
+  {
+    path: "/",
+    component: <Restaurants />,
+  },
+  {
+    path: "/search",
+    component: <Search />,
+  },
+  {
+    path: "/category/:slug",
+    component: <Category />,
+  },
+  {
+    path: "/restaurants/:id",
+    component: <Restaurant />,
+  },
+];
+
+const commonRoutes = [
+  {
+    path: "/confirm",
+    component: <ConfirmEmail />,
+  },
+  {
+    path: "/editProfile",
+    component: <EditProfile />,
+  },
+];
+
+const ownerRoutes = [
+  {
+    path: "/",
+    component: <MyRestaurants />,
+  },
 ];
 
 export const LoggedInRouter = () => {
@@ -46,7 +63,23 @@ export const LoggedInRouter = () => {
     <Router>
       <Header />
       <Switch>
-        {data.me.role === UserRole.Client && ClientRouter}
+        {data.me.role === UserRole.Client &&
+          clientRoutes.map((route) => (
+            <Route key={route.path} path={route.path}>
+              {route.component}
+            </Route>
+          ))}
+        {data.me.role === UserRole.Owner &&
+          ownerRoutes.map((route) => (
+            <Route key={route.path} path={route.path}>
+              {route.component}
+            </Route>
+          ))}
+        {commonRoutes.map((route) => (
+          <Route key={route.path} path={route.path}>
+            {route.component}
+          </Route>
+        ))}
         <Route>
           <NotFoundPage />
         </Route>
